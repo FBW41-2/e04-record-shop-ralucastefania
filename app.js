d34500bfa9424da8b21442a23e322178cdcf0859
RalucaStefania <raluca@bochis.com>
2021-04-25 14:14:33 +0200

    changed add records and getrecords

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
 /** EXTERNAL DEPENDENCIES */
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const { MongoClient } = require("mongodb");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
 /** ROUTERS */
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const recordsRouter = require('./routes/records');
-const ordersRouter = require('./routes/orders');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const recordsRouter = require("./routes/records");
+const ordersRouter = require("./routes/orders");
 const { setCors } = require("./middleware/security");
 
 /** INIT */
 const app = express();
 
 /** LOGGING */
-app.use(logger('dev'));
+app.use(logger("dev"));
 
+/** CONNECT TO MONGODB **/
+async function connectDB() {
+  const url =
+    "mongodb+srv://Raluca:7HFnyNIFLcH57sMF@cluster0.gdgwl.mongodb.net/record-shop?retryWrites=true&w=majority";
+  const client = new MongoClient(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+  try {
+    await client.connect();
+    app.locals.db = client.db("record-shop");
+    await listDatabases(client);
+  } catch (error) {
+  } finally {
+    // await client.close();
+  }
+}
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+connectDB().catch(console.error);
 
 /** SETTING UP LOWDB */
-const adapter = new FileSync('data/db.json');
-const db = low(adapter);
-db.defaults({
-    records: [],
-    users: [],
-    orders: []
-}).write();
-
+// const adapter = new FileSync('data/db.json');
+// const db = low(adapter);
+// db.defaults({
+//     records: [],
+//     users: [],
+//     orders: []
+// }).write();
 
 /** REQUEST PARSERS */
 app.use(express.json());
@@ -37,30 +64,28 @@ app.use(cookieParser());
 app.use(setCors);
 
 /** STATIC FILES*/
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 /** ROUTES */
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/records', recordsRouter);
-app.use('/orders', ordersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/records", recordsRouter);
+app.use("/orders", ordersRouter);
 
 /** ERROR HANDLING */
 app.use(function (req, res, next) {
-    const error = new Error('Looks like something broke...');
-    error.status = 400;
-    next(error);
+  const error = new Error("Looks like something broke...");
+  error.status = 400;
+  next(error);
 });
 
 app.use(function (err, req, res, next) {
-    res.send({
-        error: {
-            message: err.message
-        }
-    });
+  res.send({
+    error: {
+      message: err.message,
+    },
+  });
 });
-
 
 /** EXPORT PATH */
 module.exports = app;
--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,39 +1,41 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('data/db.json');
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("data/db.json");
 const db = low(adapter);
 
-
 exports.getRecords = (req, res, next) => {
-    const records = db.get('records').value()
-    res.status(200).send(records);
-}
+  req.app.locals.db
+    .collection("records")
+    .find()
+    .toArray((err, docs) => {
+      res.json(docs);
+    });
+};
 
 exports.getRecord = (req, res, next) => {
-    const { id } = req.params;
-    const record = db.get('records').find({ id });
-    res.status(200).send(record);
-}
+  const { id } = req.params;
+  const record = db.get("records").find({ id });
+  res.status(200).send(record);
+};
 
 exports.deleteRecord = (req, res, next) => {
-    const { id } = req.params;
-    const record = db.get('records').remove({ id }).write();
-    res.status(200).send(record);
-}
+  const { id } = req.params;
+  const record = db.get("records").remove({ id }).write();
+  res.status(200).send(record);
+};
 
 exports.updateRecord = (req, res, next) => {
-    const { id } = req.params;
-    const dt = req.body;
-    const record = db.get('records').find({ id }).assign(dt).write();
-    res.status(200).send(record);
-}
+  const { id } = req.params;
+  const dt = req.body;
+  const record = db.get("records").find({ id }).assign(dt).write();
+  res.status(200).send(record);
+};
 
 exports.addRecord = (req, res, next) => {
-    const record = req.body;
-    db.get('records').push(record)
-        .last()
-        .assign({ id: Date.now().toString() })
-        .write()
-
-    res.status(200).send(record);
-}
+  const newRecord = records.insertOne(
+    { artist: "Snoop Dog", album_name: "Dog Pound", year: 1998 },
+    (err, entry) => {
+      res.json(newRecord);
+    }
+  );
+};
